c306b1c3d428590c03fe1b7c2d787c78298093e6
FloLoveBit <flo.love.bit@gamil.com>
2022-11-21 16:53:41 +0100

    error page

--- src/app/shared/ui/errors/error/error.component.ts
+++ src/app/shared/ui/errors/error/error.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-error',
-  templateUrl: './error.component.html',
-  styleUrls: ['./error.component.scss']
+  selector: "app-error",
+  templateUrl: "./error.component.html",
+  styleUrls: ["./error.component.scss"],
 })
 export class ErrorComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private route: ActivatedRoute) {}
+  errorNum = "";
   ngOnInit(): void {
+    this.errorNum=this.route.snapshot.params['type'];
   }
-
 }
